4ebbe6750135286841d55dad48143cffe43791fe
Noel <garcia.guimeras@gmail.com>
2013-02-12 15:37:04 -0500

    Improving method calling

--- DeVil/DeVil.py
+++ DeVil/DeVil.py
@@ -12,12 +12,21 @@ class daemon(object):
 
     def __init__(self, process_name):
         self.process_name = process_name
+        self.base_commands = {
+            "start": (self._start, "Starts daemon"),
+            "stop": (self._stop, "Stops daemon"),
+            "status": (self._status, "Gets daemon status"),
+            "pid": (self._pid, "Prints pid number")
+        }
 
     def start(self):
         pass
 
     def stop(self):
         pass
+
+    def commands(self):
+        return {}
 
     ### Methods
     def _print(self, text):
@@ -76,30 +85,31 @@ class daemon(object):
         
     def _help(self):
         print("{0} daemon".format(self.process_name))
-        print("Expected arguments are:")
-        print("     start       Starts producer thread daemon")
-        print("     status      Gets producer thread status")    
-        print("     stop        Stops producer thread daemon")
-        print("     pid         Prints pid number")
-
+        print("Base arguments are:")
+        for k in self.base_commands:
+            print("    {0}    {1}".format(k, self.base_commands[k][1]))
+        commands = self.commands()
+        if len(commands) == 0:
+            return
+        print("Other arguments are:")
+        for k in commands:
+            print("    {0}    {1}".format(k, commands[k][1]))
 
     ### Main method    
     def run(self):
         argv = sys.argv[1:]
-        if len(argv) <> 1:
+        if len(argv) < 1:
             self._help()
             return
-        argv = argv[0]
-        if argv == "start":
-            self._start()
+        arg0 = argv[0]
+        args = argv[1:]
+        if arg0 in self.base_commands:
+            f = self.base_commands[arg0][0]
+            f()
             return
-        if argv == "status":
-            self._status()
-            return        
-        if argv == "stop":
-            self._stop()
+        commands = self.commands()
+        if arg0 in commands:
+            f = commands[arg0][0]
+            f(*args)
             return
-        if argv == "pid":
-            self._pid()
-            return         
         self._help()
